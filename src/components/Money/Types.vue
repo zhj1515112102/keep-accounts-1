d46455acdd4016752786de4d1520e4fc8e6d33b0
zhj <1515112102@qq.com>
2022-04-17 13:18:31 +0800

    基本完成NumberPad组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type==='-' && 'selected'"
+      @click="selectType('-')">支出</li>
+      <li :class="type==='+' && 'selected'"
+      @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Types"
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue{
+  type='-';
+  selectType(type:string){
+    if(type!=='-' && type!=='+'){
+      throw new Error('type is unknown')
+    }
+    this.type = type;
+  }
+
 }
+// export default {
+//   name: "Types"
+// }
 </script>
 
 <style lang="scss" scoped>
