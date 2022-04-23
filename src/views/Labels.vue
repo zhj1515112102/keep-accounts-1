29357af1e0c7f95a7a2ea42716c4aae084b34158
zhj <1515112102@qq.com>
2022-04-23 22:32:44 +0800

    Labels.vue使用Vuex,抽离TagHelper作为mixin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -15,22 +15,21 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
-import store from '@/store/index2';
+import TagHelper from '@/mixins/TagHelper';
+import {mixins} from 'vue-class-component';
 
 @Component({
   components: {Button}
 })
-export default class Labels extends Vue {
-  tags = store.tagList;
+export default class Labels extends mixins(TagHelper) {
+  get tags() {
+    return this.$store.state.tagList;
+  }
 
-  createTag() {
-    const name = window.prompt('请输入标签名');
-    if (name) {
-      store.createTag(name);
-    }
+  beforeCreate() {
+    this.$store.commit('fetchTags');
   }
 }
 </script>
