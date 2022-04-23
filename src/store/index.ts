d900a03ed56d498644b4d7017285c8dbdb153d67
zhj <1515112102@qq.com>
2022-04-23 22:05:43 +0800

    Money.vue使用Vuex封装

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,86 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import router from '@/router';
+import createId from '@/lib/createId';
+import clone from '@/lib/clone';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+type RootState = {
+  recordList: RecordItem[],
+  tagList: Tag[],
+  currenTag?: Tag
+}
+
+const store = new Vuex.Store({
   state: {
-  },
+    recordList: [],
+    tagList: [],
+    currenTag: undefined
+  } as RootState,
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    setCurrentTag(state, id: string) {
+      state.currenTag = state.tagList.filter(t => t.id === id)[0];
+    },
+    updateTag(state, payload: { id: string, name: string }) {
+      const {id, name} = payload;
+      const idList = state.tagList.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名重复了');
+        } else {
+          const tag = state.tagList.filter(item => item.id === id)[0];
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back();
+      } else {
+        window.alert('删除失败');
+      }
+    },
+    fetchTags(state) {
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+    },
+    createTag(state, name: string) {
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签重复');
+      }
+      const id = createId().toString();
+      state.tagList.push({id, name: name});
+      store.commit('saveTags');
+      window.alert('添加成功');
+    },
+    saveTags(state) {
+      window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
+    },
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    },
+    saveRecords(state) {
+      window.localStorage.setItem('recordList', JSON.stringify(state.recordList));
+    },
+    createRecord(state, record) {
+      const record2: RecordItem = clone(record);
+      record2.createAt = new Date();
+      state.recordList.push(record2);
+      store.commit('saveRecords');
+    },
   }
-})
+});
+
+export default store;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,7 +11,6 @@
     </div>
     <Tags @update:value="onUpdateTags"/>
   </Layout>
-
 </template>
 
 <script lang="ts">
@@ -22,21 +21,23 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import store from '@/store/index2.ts';
 
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad},
-  computed: {
-    recordList() {
-      return store.recordList;
-    }
-  }
 })
 export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
+  created() {
+    this.$store.commit('fetchRecords');
+  }
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -51,7 +52,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    store.createRecord(this.record);
+    this.$store.commit('createRecord', this.record);
   }
 }
 </script>
